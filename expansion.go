f24b8abfe2bedb90bb98b6dc94a85d698f97f8a3
DarrylHuet <DarrylHuet@gmail.com>
2022-05-30 09:49:49 -0600

    Update expansion.go
    Overview comment

--- expansion.go
+++ expansion.go
@@ -1,5 +1,17 @@
 package main
 
+/*
+Algorithm
+1. Check if p**n divides b
+2. Solve bx = 1 (mod p)
+3. If x_n is a solution then set a_n = c * x_n (mod p)
+4. Set a_n b = r(N) - r(N+1) * p (mod p)
+5. Check a_n * b = r(N) (mod p)
+6. A_n = c*r_n (mod p)
+7.if r(N+1) = r_i for any I in N then {a_0, a_1, ..., a_n} you are done
+8. If not 7, set lambda = n-i +1 and go back to 2
+*/
+
 func padic_expansion(a, b, p int64) *Padic {
   var n int64 = 1
   g := GCD(b, power(p,n))
