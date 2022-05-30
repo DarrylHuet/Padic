fb223b3baf7ff83a74914540909b6cd8177c02e9
DarrylHuet <DarrylHuet@gmail.com>
2022-05-30 09:45:18 -0600

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
   "fmt"
 )
 
+//Currently 22/7 computes to 3, since 22/7 = 3.14285714286
+//Under the Padic, our fraction should be an exact computation with specific precision
 func main() {
   var p int64 = 13
   var a int64 = 22
